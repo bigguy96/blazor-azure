72823c957678220072016d2c71bd49805e0d2ed8
GT <26967905+bigguy96@users.noreply.github.com>
2021-01-19 00:04:34 -0500

    Work in progress with work items.

--- ClassLibrary/Services/AzureWorkItemsService.cs
+++ ClassLibrary/Services/AzureWorkItemsService.cs
@@ -2,7 +2,9 @@
 using Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -17,7 +19,7 @@ namespace ClassLibrary.Services
         {
             _httpClient = httpClientFactory.CreateClient(MyClient);
         }
-        
+
         public async Task<Iteration> GetIterations()
         {
             const string uri = "myTC%20Account%20Team/_apis/work/teamsettings/iterations?api-version=6.0";
@@ -27,14 +29,40 @@ namespace ClassLibrary.Services
             return JsonSerializer.Deserialize<Iteration>(json);
         }
 
-        public Task<WorkItem> GetWorkItemDetails(IEnumerable<int> ids)
+        public async Task<WorkItem> GetWorkItemDetails(IEnumerable<int> ids)
         {
-            throw new NotImplementedException();
+            const string uri = "_apis/wit/workitemsbatch?api-version=6.0";
+            var batch = new WorkItemsBatch
+            {
+                ids = ids.ToArray(),
+                fields = new[]
+                {
+                    "System.Id",
+                    "System.Title",
+                    "System.WorkItemType",
+                    "System.IterationPath",
+                    "System.AssignedTo",
+                    "Microsoft.VSTS.TCM.ReproSteps",
+                    "Microsoft.VSTS.Common.AcceptanceCriteria",
+                    "System.Description"
+                }
+            };
+            var json = JsonSerializer.Serialize(batch);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(uri, content);
+
+            var t = await response.Content.ReadAsStringAsync();
+
+            return null;
         }
 
-        public Task<WorkItemIteration> GetWorkItems(string id)
+        public async Task<WorkItemIteration> GetWorkItems(string id)
         {
-            throw new NotImplementedException();
+            var uri = $"myTC%20Account%20Team/_apis/work/teamsettings/iterations/{id}/workitems?api-version=6.0-preview.1";
+            var response = await _httpClient.GetAsync(uri);
+            var json = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<WorkItemIteration>(json);
         }
     }
 }
---
+++ Entities/WorkItemsBatch.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities
+{
+    public class WorkItemsBatch
+    {
+        public int[] ids { get; set; }
+        public string[] fields { get; set; }
+    }
+}
